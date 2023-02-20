fda4522f424c8cd9b515626b8431db9659da4015
Adrian Campana <adrianphcampana12@gmail.com>
2023-02-20 19:38:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1 - clica no botão de sortear mensagem
+// 2 - script recebe a ação do click
+// 3 - script esconde o bloco "front"
+// 3 - script mostra o bloco "back" onde vai conter a mensagem
+// 4 - o script sorteia um número de 1-9
+// --- varias mensagens pré-criadas no html com classes de 1-9 escondidas com display none
+// --- o script sorteia um número de 1-9 e tira o display none da classe sorteada
+// 4 - a mensagem aparece na aplicação
+// 5 - quando clicar no botão "abrir outro biscoito", sorteia novamente outra mensagem
